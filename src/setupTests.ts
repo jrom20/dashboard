b0c802715a6d16f565711cc694a5dcf7a85db226
jrom20 <jrom20@users.noreply.github.com>
2017-07-03 16:16:01 -0600

    Update setupTests.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,2 @@
-process.env.REACT_APP_APP_INSIGHTS_APPID = "APPID";
-process.env.REACT_APP_APP_INSIGHTS_APIKEY = "APIKEY";
+process.env.REACT_APP_APP_INSIGHTS_APPID = "334adca0-c6c5-4357-9061-49619a49bd81";
+process.env.REACT_APP_APP_INSIGHTS_APIKEY = "ch21ei3bf5r7zruuk6qbsjcfxigl0ef52nfjieta";
